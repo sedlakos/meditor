5475dc565426876a90c5888425019d7a0ff17499
Antonin Sajboch <sajboch@gmail.com>
2021-02-20 23:50:59 +0100

    fixed autoimport

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Provider} from "unstated";
 import {EditorContainer} from "./model/EditorContainer";
 import {ContainerField} from "./components/fields/ContainerField";
 import "./components/fields/TextField"
+import "./components/fields/ContainerField"
 
 function App() {
     let editorContainer = new EditorContainer();
